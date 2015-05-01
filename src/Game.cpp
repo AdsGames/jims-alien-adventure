00f5adf8909e75c6b288ac80119c829e993d61cc
Allan Legemaate <alegemaate@gmail.com>
2015-05-01 15:07:43 -0400

    Stairs!

--- src/Game.cpp
+++ src/Game.cpp
@@ -4,37 +4,6 @@ volatile int Game::timer1 = 00;
 
 Game::Game()
 {
-  // Init fmod
-  FSOUND_Init (44100, 32, 0);
-
-  // Creates a random number generator (based on time)
-  srand (time(NULL));
-
-  // Other Sprites
-  buffer = create_bitmap( SCREEN_W, SCREEN_H);
-
-  // Temporary fonts
-  /*FONT *f1, *f2, *f3, *f4, *f5;
-
-  //Sets Font
-  if(!(f1 = load_font(("fonts/cooper_black.pcx"), NULL, NULL))){
-    abort_on_error( "Cannot find font fonts/arial_black.png \n Please check your files and try again");
-  }
-
-  f2 = extract_font_range(f1, ' ', 'A'-1);
-  f3 = extract_font_range(f1, 'A', 'Z');
-  f4 = extract_font_range(f1, 'Z'+1, 'z');
-
-  //Merge fonts
-  cooper = merge_fonts(f4, f5 = merge_fonts(f2, f3));
-
-  //Destroy temporary fonts
-  destroy_font(f1);
-  destroy_font(f2);
-  destroy_font(f3);
-  destroy_font(f4);
-  destroy_font(f5);*/
-
   // Init
   init();
 }
@@ -45,15 +14,64 @@ void Game::gameTicker(){
 END_OF_FUNCTION(gameTicker)
 
 void Game::init(){
+  // Init fmod
+  FSOUND_Init (44100, 32, 0);
 
+  // Creates a random number generator (based on time)
+  srand (time(NULL));
+
+  // Load images
+  if(!(stairs = load_bitmap( "images/stairs.png", NULL))){
+    abort_on_error( "Cannot find image images/stairs.png \n Please check your files and try again");
+  }
+
+  // Other Sprites
+  buffer = create_bitmap( SCREEN_W, SCREEN_H);
+
+  // Temporary fonts
+  FONT *f1, *f2, *f3, *f4, *f5;
+
+  //Sets Font
+  if(!(f1 = load_font(("fonts/arial_black.pcx"), NULL, NULL))){
+    abort_on_error( "Cannot find font fonts/arial_black.png \n Please check your files and try again");
+  }
+
+  f2 = extract_font_range(f1, ' ', 'A'-1);
+  f3 = extract_font_range(f1, 'A', 'Z');
+  f4 = extract_font_range(f1, 'Z'+1, 'z');
+
+  //Merge fonts
+  font = merge_fonts(f4, f5 = merge_fonts(f2, f3));
+
+  //Destroy temporary fonts
+  destroy_font(f1);
+  destroy_font(f2);
+  destroy_font(f3);
+  destroy_font(f4);
+  destroy_font(f5);
 }
 
+// Update game state
 void Game::update(){
 
 }
 
+// Draw game state
 void Game::draw(){
+  // Background
+  rectfill( buffer, 0, 0, SCREEN_W, SCREEN_H, makecol( 255, 255, 255));
 
+  // Stairs
+  for( int i = 0; i < SCREEN_W; i += (stairs -> w)/2)
+    draw_sprite( buffer, stairs, i, SCREEN_W - location_y(i));
+
+  // Buffer
+  draw_sprite( screen, buffer, 0, 0);
+}
+
+// Line y position
+float Game::location_y( int oldX){
+  return oldX;
 }
 
 Game::~Game()
