464180378ea1c91dfbb6ef093c3c2747f3ad67a5
Allan Legemaate <alegemaate@gmail.com>
2015-05-01 19:09:26 -0400

    DanniesNose!

--- src/key_manager.cpp
+++ src/key_manager.cpp
@@ -1,5 +1,8 @@
 #include "key_manager.h"
 
+BITMAP* key_manager::keys[255];
+
+// Init
 key_manager::key_manager(int newX, int newY){
   // Set position
   x = newX;
@@ -24,5 +27,6 @@ void key_manager::draw( BITMAP *tempImage){
   // Draw keys
   for( unsigned int i = 0; i < key_queue.size(); i++){
     rectfill( tempImage, i * 40 + x, y, i * 40 + 30, y + 30, makecol( 255,0,0));
+    draw_sprite( tempImage, keys[KEY_UP],i * 40 + x, y);
   }
 }
