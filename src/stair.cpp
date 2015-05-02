6613d84ab1d91f11628c94430e33f099b46be079
danwardvs <danwardvs@adsgames.net>
2015-05-02 08:58:49 -0400

    Started on end stage loading

--- src/stair.cpp
+++ src/stair.cpp
@@ -1,14 +1,17 @@
 #include "stair.h"
+#include "globals.h"
 
 int stair::numberStairs;
 const int stair::maxScrollSpeed = 6;
 float stair::scrollSpeed;
 BITMAP* stair::image;
 BITMAP* stair::image_brick;
+BITMAP* stair::stage_end_red;
 
-stair::stair(float newX, float newY){
+stair::stair(float newX, float newY, int newType){
   x = newX;
   y = newY;
+  type = newType;
 
   stairID = numberStairs;
   numberStairs++;
@@ -21,7 +24,7 @@ stair::~stair(){
 // Update those stairs
 void stair::update( vector<stair>* allStairsCopy){
   // reset stairs
-  if( y > SCREEN_H + image -> h){
+  if( y > SCREEN_H + image -> h && !is_game_done){
     x = allStairsCopy -> at(find_bottom_stair(stairID)).x + 30;
     y = location_y(x);
   }
@@ -34,10 +37,13 @@ void stair::movement(){
 }
 
 // Draw those stairs
+// YES WE CAN
 void stair::draw( BITMAP *tempImage){
   // Draw stair and rectangle beside for effect
-  for( int i = x + image -> w - 30; i < SCREEN_W; i+= image_brick -> w)
-    draw_sprite( tempImage, image_brick, i, y + image -> h);
+  for( int i = x + image -> w - 30; i < SCREEN_W; i+= image_brick -> w){
+    if(type==0)draw_sprite( tempImage, image_brick, i, y + image -> h);
+    if(type==1)draw_sprite( tempImage, stage_end_red, i, y + image -> h);
+  }
   draw_sprite( tempImage, image, x, y);
 }
 
