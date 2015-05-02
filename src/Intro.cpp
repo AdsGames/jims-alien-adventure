b47ba7ffdade7e70c3178be86d637c645b99caf5
danwardvs <danwardvs@adsgames.net>
2015-05-02 15:36:09 -0400

    Splash screen

--- src/Intro.cpp
+++ src/Intro.cpp
@@ -5,6 +5,10 @@ Intro::Intro()
 {
   // Buffer
   buffer = create_bitmap( SCREEN_W, SCREEN_H);
+
+  if(!(splash = load_bitmap( "images/splash.png", NULL))){
+    abort_on_error( "Cannot find image images/splash.png \n Please check your files and try again");
+  }
 }
 
 void Intro::update(){
@@ -12,8 +16,7 @@ void Intro::update(){
 
 void Intro::draw(){
   // Fade in
-  //highcolor_fade_in( IMAGE, 32);
-
+  highcolor_fade_in( splash, 16);
   // Background
   rectfill( buffer, 0, 0, SCREEN_W, SCREEN_W, makecol( 255, 255, 255));
 
@@ -21,16 +24,17 @@ void Intro::draw(){
   textprintf_ex( buffer, font, 0, 130, makecol(255,255,255), makecol(0,0,0), "INTRO!");
 
   // Draw Buffer
+  stretch_sprite(buffer, splash, 0,0,SCREEN_W,SCREEN_H);
   draw_sprite( screen, buffer, 0, 0);
 
   // Wait
   rest( 2000);
 
   // Fade out
-  highcolor_fade_out( 64);
+  highcolor_fade_out( 16);
 
   // Wait 2 seconds then go to the menu
-  set_next_state(STATE_MENU);
+  set_next_state(STATE_GAME);
 }
 
 Intro::~Intro()
