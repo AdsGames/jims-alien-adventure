4794b1d222f87ccb428949f329cbfd168955e682
Allan Legemaate <alegemaate@gmail.com>
2015-06-25 18:12:20 -0400

    Removed Lucas

--- src/location.cpp
+++ src/location.cpp
@@ -35,8 +35,6 @@ void location::draw( BITMAP *tempImage){
     draw_trans_sprite( tempImage, pin_image, x, y);
   else if(name=="wall_of_china" && !beaten_levels[6])
     draw_trans_sprite( tempImage, pin_image, x, y);
- else if(name=="lucas" && !beaten_levels[7])
-    draw_trans_sprite( tempImage, pin_image, x, y);
 
 
   if(name=="cn_tower" && beaten_levels[0])
@@ -50,8 +48,6 @@ void location::draw( BITMAP *tempImage){
   else if(name=="taj_mahal" && beaten_levels[4])
     draw_trans_sprite( tempImage, pin_grey_image, x, y);
   else if(name=="wall_of_china" && beaten_levels[6])
-    draw_trans_sprite( tempImage, pin_grey_image, x, y);
-    else if(name=="lucas" && beaten_levels[7])
     draw_trans_sprite( tempImage, pin_grey_image, x, y);
 
   // Image
